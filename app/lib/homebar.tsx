4ec4cbbd64a62027bc73e36e3b4d21184bf0d0a6
kayreo <kayla.han@g.austincc.edu>
2025-01-11 18:56:07 -0600

    out folder

--- app/lib/homebar.tsx
+++ app/lib/homebar.tsx
@@ -19,6 +19,5 @@ export default function HomeBar() {
         <p className="text-xl pb-[2%]">I primarily program in languages such as Python, C++, C#, and Java. I also have experience working on projects that use Javascript and Swift! Some other technologies I'm familiar with are the Unity, Godot, and Unreal engines, along with WebGL and Jupyter Notebook.</p>
         <p className="text-xl pb-[2%]">I am an active participant in the UTCS community. I have held multiple leadership positions in the UTCS department, such as being a TA for courses that teach nonmajor computer science students in topics such as data structures and graphics, a mentor for first-year UTCS students, and a summer camp counsellor for the UTCS Summer Academies. I also have participated in events and hackathons run by student organizations like Women in Computer Science (WiCS).</p>
         <p className="text-xl pb-[2%]">My main interests are software engineering and gameplay programming. I hope to find a job where I can use the technical and personal skills I have acquired at UT Austin in an exciting, impactful project that changes the world. Aside from technical prowess, community is extremely important to me. I love working in a diverse, motivated team where I can learn and grow into an experienced software developer.</p>
-        <p className="text-4xl">-</p>
     </div>;
 }
